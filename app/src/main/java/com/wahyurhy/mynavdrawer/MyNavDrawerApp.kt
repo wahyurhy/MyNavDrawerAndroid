e833630afa432c783ac82c537c432299b23dfdda
wahyurhy <wahyurahayu82@gmail.com>
2022-11-30 02:12:46 +0700

    up to Latihan State Holder pada Navigation Drawer

--- app/src/main/java/com/wahyurhy/mynavdrawer/MyNavDrawerApp.kt
+++ app/src/main/java/com/wahyurhy/mynavdrawer/MyNavDrawerApp.kt
@@ -1,6 +1,5 @@
 package com.wahyurhy.mynavdrawer
 
-import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.foundation.background
@@ -17,62 +16,31 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.wahyurhy.mynavdrawer.ui.theme.MyNavDrawerTheme
-import kotlinx.coroutines.launch
 
 @Composable
 fun MyNavDrawerApp() {
-    val scaffoldState = rememberScaffoldState()
-    val scope = rememberCoroutineScope()
-    val context = LocalContext.current
+
+    val appState = rememberMyNavDrawerState()
 
     Scaffold(
-        scaffoldState = scaffoldState,
+        scaffoldState = appState.scaffoldState,
         topBar = {
             MyTopBar(
-                onMenuClick = {
-                    scope.launch {
-                        scaffoldState.drawerState.open()
-                    }
-                }
+                onMenuClick = appState::onMenuClick
             )
         },
         drawerContent = {
             MyDrawerContent(
-                onItemSelected = { title ->
-                    scope.launch {
-                        scaffoldState.drawerState.close()
-                        val snackbarResult = scaffoldState.snackbarHostState.showSnackbar(
-                            message = context.resources.getString(R.string.coming_soon, title),
-                            actionLabel = context.resources.getString(R.string.subscribe_question)
-                        )
-                        if (snackbarResult == SnackbarResult.ActionPerformed) {
-                            Toast.makeText(
-                                context,
-                                context.resources.getString(R.string.subscribed_info),
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        } else if (snackbarResult == SnackbarResult.Dismissed) {
-                            Toast.makeText(context, "Thnak You!", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-
-                },
-                onBackpress = {
-                    if (scaffoldState.drawerState.isOpen) {
-                        scope.launch {
-                            scaffoldState.drawerState.close()
-                        }
-                    }
-                },
+                onItemSelected = appState::onItemSelected,
+                onBackpress = appState::onBackpress,
             )
         },
-        drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
+        drawerGesturesEnabled = appState.scaffoldState.drawerState.isOpen,
     ) { paddingValues ->
         Box(
             modifier = Modifier
---
+++ app/src/main/java/com/wahyurhy/mynavdrawer/MyNavDrawerState.kt
@@ -0,0 +1,62 @@
+package com.wahyurhy.mynavdrawer
+
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.SnackbarResult
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+class MyNavDrawerState(
+    val scaffoldState: ScaffoldState,
+    private val scope: CoroutineScope,
+    private val context: Context
+) {
+    fun onMenuClick() {
+        scope.launch {
+            scaffoldState.drawerState.open()
+        }
+    }
+
+    fun onItemSelected(title: String) {
+        scope.launch {
+            scaffoldState.drawerState.close()
+            val snackbarResult = scaffoldState.snackbarHostState.showSnackbar(
+                message = context.resources.getString(R.string.coming_soon, title),
+                actionLabel = context.resources.getString(R.string.subscribe_question)
+            )
+            if (snackbarResult == SnackbarResult.ActionPerformed) {
+                Toast.makeText(
+                    context,
+                    context.resources.getString(R.string.subscribed_info),
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else if (snackbarResult == SnackbarResult.Dismissed) {
+                Toast.makeText(context, "Thnak You!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun onBackpress() {
+        if (scaffoldState.drawerState.isOpen) {
+            scope.launch {
+                scaffoldState.drawerState.close()
+            }
+        }
+    }
+}
+
+@Composable
+fun rememberMyNavDrawerState(
+    scaffoldState: ScaffoldState = rememberScaffoldState(),
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    context: Context = LocalContext.current
+): MyNavDrawerState =
+    remember(scaffoldState, coroutineScope, context) {
+        MyNavDrawerState(scaffoldState, coroutineScope, context)
+    }
