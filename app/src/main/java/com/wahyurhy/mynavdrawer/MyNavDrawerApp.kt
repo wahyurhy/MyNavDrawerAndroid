5ca08bb34a1e676447fe1cf28108bd05ae4c6572
wahyurhy <wahyurahayu82@gmail.com>
2022-11-29 23:34:08 +0700

    make navigation drawer with handler backpress and add one form for name

--- app/src/main/java/com/wahyurhy/mynavdrawer/MyNavDrawerApp.kt
+++ app/src/main/java/com/wahyurhy/mynavdrawer/MyNavDrawerApp.kt
@@ -1,40 +1,118 @@
 package com.wahyurhy.mynavdrawer
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.wahyurhy.mynavdrawer.ui.theme.MyNavDrawerTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun MyNavDrawerApp() {
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+
     Scaffold(
+        scaffoldState = scaffoldState,
         topBar = {
             MyTopBar(
                 onMenuClick = {
-
+                    scope.launch {
+                        scaffoldState.drawerState.open()
+                    }
                 }
             )
         },
+        drawerContent = {
+            MyDrawerContent(
+                onItemSelected = { title ->
+                    scope.launch {
+                        scaffoldState.drawerState.close()
+                        val snackbarResult = scaffoldState.snackbarHostState.showSnackbar(
+                            message = context.resources.getString(R.string.coming_soon, title),
+                            actionLabel = context.resources.getString(R.string.subscribe_question)
+                        )
+                        if (snackbarResult == SnackbarResult.ActionPerformed) {
+                            Toast.makeText(
+                                context,
+                                context.resources.getString(R.string.subscribed_info),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else if (snackbarResult == SnackbarResult.Dismissed) {
+                            Toast.makeText(context, "Thnak You!", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                },
+                onBackpress = {
+                    if (scaffoldState.drawerState.isOpen) {
+                        scope.launch {
+                            scaffoldState.drawerState.close()
+                        }
+                    }
+                },
+            )
+        },
+        drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
     ) { paddingValues ->
         Box(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues),
-            contentAlignment = Alignment.Center
+//            contentAlignment = Alignment.Center
         ) {
-            Text(text = stringResource(id = R.string.hello_world))
+            MyForm()
         }
 
     }
 }
+
+@Composable
+fun MyForm() {
+    val input = rememberFormInputState(input = "")
+    FormInput(state = input)
+}
+
+@Composable
+fun FormInput(
+    state: FormInputState = rememberFormInputState(input = ""),
+) {
+    OutlinedTextField(
+        value = state.input,
+        onValueChange = { state.input = it },
+        label = { Text(text = "Nama") },
+        modifier = Modifier.padding(8.dp)
+    )
+}
+
+class FormInputState(initialInput: String) {
+    var input by mutableStateOf(initialInput)
+}
+
+@Composable
+fun rememberFormInputState(input: String): FormInputState =
+    remember(input) {
+        FormInputState(input)
+    }
 
 @Composable
 fun MyTopBar(onMenuClick: () -> Unit) {
@@ -53,6 +131,85 @@ fun MyTopBar(onMenuClick: () -> Unit) {
             Text(text = stringResource(id = R.string.app_name))
         },
     )
+}
+
+data class MenuItem(val title: String, val icon: ImageVector)
+
+@Composable
+fun MyDrawerContent(
+    modifier: Modifier = Modifier,
+    onItemSelected: (title: String) -> Unit,
+    onBackpress: () -> Unit,
+) {
+    val items = listOf(
+        MenuItem(
+            title = stringResource(id = R.string.home),
+            icon = Icons.Default.Home
+        ),
+        MenuItem(
+            title = stringResource(id = R.string.favourite),
+            icon = Icons.Default.Favorite
+        ),
+        MenuItem(
+            title = stringResource(id = R.string.profile),
+            icon = Icons.Default.AccountCircle
+        ),
+    )
+    Column(modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .height(200.dp)
+                .fillMaxWidth()
+                .background(MaterialTheme.colors.primary)
+        )
+        for (item in items) {
+            Row(modifier = Modifier
+                .clickable { onItemSelected(item.title) }
+                .padding(vertical = 12.dp, horizontal = 16.dp)
+                .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    imageVector = item.icon,
+                    contentDescription = item.title,
+                    tint = Color.DarkGray
+                )
+                Spacer(modifier = Modifier.width(32.dp))
+                Text(text = item.title, style = MaterialTheme.typography.subtitle2)
+            }
+        }
+    }
+    BackPressHandler {
+        onBackpress()
+    }
+    Divider()
+}
+
+@Composable
+fun BackPressHandler(enabled: Boolean = true, onBackPressed: () -> Unit) {
+    val currentOnBackPressed by rememberUpdatedState(newValue = onBackPressed)
+    val backCallback = remember {
+        object : OnBackPressedCallback(enabled) {
+            override fun handleOnBackPressed() {
+                currentOnBackPressed()
+            }
+
+        }
+    }
+    SideEffect {
+        backCallback.isEnabled = enabled
+    }
+
+    val backDispatcher = checkNotNull(LocalOnBackPressedDispatcherOwner.current) {
+        "No OnBackPressedDispatcherOwner was provided via LocalOnBackPressedDispatcherOwner"
+    }.onBackPressedDispatcher
+
+    val lifecycleOwner = LocalLifecycleOwner.current
+    DisposableEffect(lifecycleOwner, backDispatcher) {
+        backDispatcher.addCallback(lifecycleOwner, backCallback)
+        onDispose {
+            backCallback.remove()
+        }
+    }
 }
 
 @Preview(showBackground = true)
